3ed8fd445df039ae6ba13b9fe7aa11371bff7f5d
Mir Sahib <tusherexpress@gmail.com>
2018-12-02 09:48:33 +0000

    Assignment B

---
+++ B/part_b.c
@@ -0,0 +1,41 @@
+#include <sys/ipc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int main(){
+    int processPipe[6];
+    char writemessage[3][100];
+    char readmessage[100];
+    //initializing pipe
+    for(int i=0;i<3;i++){
+        pipe(&processPipe[2*i]);
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        if(fork()==0){
+            printf("writing child process %d\n",i);
+            int j=0;
+            while(1){
+                char in;
+                if(in == '#'){
+                    write(processPipe[2*i+1],writemessage[i],sizeof(writemessage[i]));
+                    exit(0);
+                }else{
+                    scanf("%c",&in);
+                    writemessage[i][j]=in;
+                    j++;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        wait(NULL);
+        read(processPipe[2*i],readmessage,sizeof(readmessage));
+        printf("Message from child process %d Message: %s",i,readmessage);
+    }
+}
+//final commit
